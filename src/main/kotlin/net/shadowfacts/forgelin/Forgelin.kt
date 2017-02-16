106c697b49c07f4af683fac42b1d6b16c776985d
Shadowfacts <me@shadowfacts.net>
2017-02-15 19:21:37 -0500

    Add mod, remove loading plugin, bump version Closes #5

---
+++ src/main/kotlin/net/shadowfacts/forgelin/Forgelin.kt
@@ -0,0 +1,15 @@
+package net.shadowfacts.forgelin
+
+import net.minecraftforge.fml.common.Mod
+
+/**
+ * @author shadowfacts
+ */
+@Mod(modid = Forgelin.MOD_ID, name = Forgelin.NAME, version = Forgelin.VERSION, acceptableRemoteVersions = "*", acceptedMinecraftVersions = "*", modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter")
+object Forgelin {
+
+	const val MOD_ID = "forgelin"
+	const val NAME = "Forgelin"
+	const val VERSION = "1.3.0"
+
+}
