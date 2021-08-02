5cb47eed7c3d4ea5e4bc0997b81365ec1ff24205
Licious17 <goncalo.pereira.santos@hotmail.com>
2021-08-02 15:54:39 +0100

    Updated Kotlin to 1.5.21

--- src/main/kotlin/net/shadowfacts/forgelin/Forgelin.kt
+++ src/main/kotlin/net/shadowfacts/forgelin/Forgelin.kt
@@ -14,7 +14,7 @@ object Forgelin {
 
 	const val MOD_ID = "forgelin"
 	const val NAME = "Forgelin"
-	const val VERSION = "@VERSION@"
+	const val VERSION = "1.9.0"
 
 	@EventHandler
 	fun onPreInit(event: FMLPreInitializationEvent) {
